8d6eec9da1f032e12bf81424c162cdef060a81ab
Somne <favourite_king@mail.ru>
2021-04-19 15:15:23 +0700

    Декомпозирован код из лекции

---
+++ ClassUnit.h
@@ -0,0 +1,28 @@
+#ifndef CLASSUNIT_H
+#define CLASSUNIT_H
+#include "Unit.h"
+#include <vector>
+
+class ClassUnit : public Unit
+{
+public:
+    enum AccessModifier
+    {
+        PUBLIC,
+        PROTECTED,
+        PRIVATE,
+        DEFAULT
+    };
+    static const std::vector< std::string > ACCESS_MODIFIERS;
+public:
+    explicit ClassUnit(const std::string& name, Flags modifier = AccessModifier::DEFAULT);
+    void add(const std::shared_ptr< Unit >& unit, Flags flags);
+    std::string compile(unsigned int level = 0) const = 0;
+protected:
+    std::string m_name;
+    Flags m_modifier;
+    using Fields = std::vector< std::shared_ptr< Unit > >;
+    std::vector< Fields > m_fields;
+};
+
+#endif // CLASSUNIT_H
