3842df91d91712ed0cf037d00e02a013fbf2daba
Somne <favourite_king@mail.ru>
2021-04-21 15:39:23 +0700

    Добавлена в main реализация компилирования для Java

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <memory>
 #include "cgen.h"
 #include "generate.h"
+#include "javagen.h"
 
 using std::cout;
 using std::cin;
@@ -8,8 +9,15 @@ using std::cin;
 int main()
 {
     cout << "CPlus generator:" << std::endl;
+    cout << std::endl;
     Generate Cplus(std::make_shared<GenerateCplus>());
     cout << Cplus.generate() << std::endl;
+
+    cout << "Java generator:" << std::endl;
+    cout << std::endl;
+    Generate Java(std::make_shared<GenerateJava>());
+    cout << Java.generate() << std::endl;
+
     return 0;
 }
 
