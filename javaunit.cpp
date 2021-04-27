60b1ea3ff06f578c2df1040ae0f41c7652370242
Somne <favourite_king@mail.ru>
2021-04-27 15:31:56 +0700

    Изменение в классах Unit для Java
    Добавлены модификаторы

--- javaunit.cpp
+++ javaunit.cpp
@@ -7,19 +7,35 @@ JavaClassUnit::~JavaClassUnit() {}
 
 std::string JavaClassUnit::compile(unsigned int level) const
 {
-    std::string result = generateShift(level) + "class " + m_name + " {\n";
+    std::string result = generateShift(level);
+
+
+    if (m_modifier & PRIVATE)
+        result += "private ";
+    else if (m_modifier & PROTECTED)
+        result += "protected ";
+    else
+        result += "public ";
+
+    if (m_modifier & FINAL)
+        result += "final ";
+
+    else if (m_modifier & ABSTRACT)
+        result += "abstract ";
+
+    result += "class " + m_name + " ";
+
+    result += " {\n";
 
     for (size_t i = 0; i < ACCESS_MODIFIERS.size(); ++i)
     {
         if (m_fields[i].empty())
             continue;
 
-        result += ACCESS_MODIFIERS[i] + ":\n";
-
         for (const auto& f : m_fields[i])
         {
             if (f)
-                result += f->compile(level + 1);
+                result += generateShift(level + 1) + ACCESS_MODIFIERS[i] + " " + f->compile(level);
         }
         result += "\n";
     }
@@ -38,22 +54,19 @@ std::string JavaMethodUnit::compile(unsigned int level) const
 {
     std::string result = generateShift(level);
 
-    if (m_flags & STATIC)
-    {
-        result += "static ";
-    }
+    // Методы abstract никогда не могут быть final или static
 
-    else if (m_flags & VIRTUAL)
-    {
-            result += "virtual ";
-    }
+    if (m_flags & STATIC)
+        result += "static ";
+
+    else if (m_flags & FINAL)
+        result += "final ";
+
+    else if (m_flags & ABSTRACT)
+        result += "abstract ";
 
     result += m_returnType + " ";
     result += m_name + "()";
-    if (m_flags & CONST)
-    {
-        result += " const";
-    }
     result += " {\n";
 
     for (const auto& b : m_body)
